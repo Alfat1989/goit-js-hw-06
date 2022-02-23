3ceb27acecf1a0159b0407cf23254ee75c5fa2e2
Alfat1989 <gilmalf89@gmail.com>
2022-02-24 01:08:01 +0300

    Завершение 2 задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listEl = document.querySelector("#ingredients")
+console.log(listEl)
+const itemsEl = [];
+
+const itemInnerEl = ingredients.map(el => {
+  const itemEl = document.createElement("li")
+  itemEl.classList.add("item")
+  itemEl.textContent = el
+  itemsEl.push(itemEl)
+
+//  console.log(itemEl)
+  
+})
+
+console.log(itemInnerEl)
+
+listEl.append(...itemsEl)
+
