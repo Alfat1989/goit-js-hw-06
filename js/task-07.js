c798f386631279e26f1529601bf45ea4d67117dc
Alfat1989 <gilmalf89@gmail.com>
2022-02-28 23:59:52 +0300

    завершена задача 8

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector("#font-size-control")
+
+
+const spanEl = document.querySelector("#text")
+
+const changeSize = (event)=> {
+    // console.log(event.currentTarget.value)
+    spanEl.style.fontSize=`${event.currentTarget.value}px`
+}
+
+inputEl.addEventListener("input", changeSize)
+
+
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector("form");
+console.log(form)
+
+form.addEventListener("submit", onFormSubmit)
+
+function onFormSubmit(event) {
+    // console.log("отправлено")
+    event.preventDefault()
+    
+    const inputEl = event.currentTarget.elements
+    const formObj = {}
+    // return inputEl.email.value === "" || inputEl.password.value === "" ?
+    //     window.alert("Все поля должны быть заполнены") : 
+    //     formObj.email = inputEl.email.value,
+    //     formObj.password=inputEl.password.value
+     if (inputEl.email.value==="" || inputEl.password.value==="") {
+        window.alert("Все поля должны быть заполнены")
+    } else {
+        formObj.email=inputEl.email.value
+        formObj.password=inputEl.password.value
+         
+        
+    }
+    
+    console.log(formObj)
+    
+    
+    
+}  
+
 
