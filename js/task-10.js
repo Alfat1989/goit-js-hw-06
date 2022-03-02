7db45994c80be3280716f30dbe089346358b06aa
Alfat1989 <gilmalf89@gmail.com>
2022-03-02 17:22:13 +0300

    Завершение 10 задачи

--- js/task-10.js
+++ js/task-10.js
@@ -2,4 +2,44 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const divEl=document.querySelector("#boxes")
+const createBtn = document.querySelector("button[data-create]")
+const destroyBtn = document.querySelector("button[data-destroy]")
+const inputEl = document.querySelector("input")
+
+const boxSize = 30;
+const boxIncrease = 10;
+let divBoxes = [];
+
+function createBox() {
+  
+  for (let i = 0; i < inputEl.value; i += 1){
+    
+    const box = document.createElement("div");
+    box.style.width=`${boxSize+i*boxIncrease}px`
+    box.style.height = `${boxSize+i*boxIncrease}px`
+    box.style.backgroundColor = getRandomHexColor();
+    divBoxes.push(box)
+
+    
+  }
+  
+}
+
+
+function appendBoxes() {
+  divEl.append(...divBoxes)
+}
+
+
+function clearAll() {
+  divBoxes = [];
+  divEl.innerHTML = '';
+}
+
+
+createBtn.addEventListener("click", createBox)
+createBtn.addEventListener("click", appendBoxes)
+destroyBtn.addEventListener("click", clearAll)
+
 
