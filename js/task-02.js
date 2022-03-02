971fb0e577e0be7b6bd3a321e77271c8b0358959
Alfat1989 <gilmalf89@gmail.com>
2022-03-02 14:28:12 +0300

    корректировка 3 задания

--- js/task-02.js
+++ js/task-02.js
@@ -10,14 +10,14 @@ const ingredients = [
 
 const listEl = document.querySelector("#ingredients")
 console.log(listEl)
-const itemsEl = [];
+
 
 const itemInnerEl = ingredients.map(el => {
   const itemEl = document.createElement("li")
   itemEl.classList.add("item")
   itemEl.textContent = el
-  itemsEl.push(itemEl)
+  return itemEl
 })
 
-listEl.append(...itemsEl)
+listEl.append(...itemInnerEl)
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+const btn = document.querySelector(".change-color")
+const nameBgColor=document.querySelector(".color")
+
+btn.addEventListener("click", bgColorChange)
+
+function bgColorChange() {
+  const bgColor = getRandomHexColor()
+  // console.log(bgColor)
+  document.body.style.backgroundColor = bgColor
+  nameBgColor.textContent=bgColor
+}
+
+
 function getRandomHexColor() {
+ 
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
