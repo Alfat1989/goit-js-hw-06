eb6eae1972734c062f45ff0674aa1262361f4bf9
Alfat1989 <gilmalf89@gmail.com>
2022-03-07 20:32:37 +0300

    убирает ненужный код с 8дз

--- js/task-08.js
+++ js/task-08.js
@@ -4,15 +4,12 @@ console.log(form)
 form.addEventListener("submit", onFormSubmit)
 
 function onFormSubmit(event) {
-    // console.log("отправлено")
+    
     event.preventDefault()
     
     const inputEl = event.currentTarget.elements
     const formObj = {}
-    // return inputEl.email.value === "" || inputEl.password.value === "" ?
-    //     window.alert("Все поля должны быть заполнены") : 
-    //     formObj.email = inputEl.email.value,
-    //     formObj.password=inputEl.password.value
+   
      if (inputEl.email.value==="" || inputEl.password.value==="") {
         return window.alert("Все поля должны быть заполнены")
          
